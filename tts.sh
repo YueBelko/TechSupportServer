9448dcb9a541268f02bb64ad9b6a882f32ff4e92
iris <yuebelo4ko@gmail.com>
2021-04-22 09:05:19 +0300

    add call_list

--- tts.sh
+++ tts.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd /home/user/techsupport
+# ps aux
+# kill
+#
+git clone https://YueBelko:Asakura31337@github.com/YueBelko/TechSupportServer.git
+cd TechSupportServer
 export FLASK_APP=run.py
 export FLASK_DEBUG=1
-flask run --host=0.0.0.0 > log.txt > /dev/null 2>&1 &
+# flask db migrate
+# flask db upgrade
+nohup flask run --host=0.0.0.0 > log.txt > /dev/null 2>&1 &
